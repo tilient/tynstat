376fd99758fe793946770d482cd36be26c158785
wiffel <wiffel@tilient.org>
2013-04-09 01:11:26 +0200

    rearrange files

--- make.sh
+++ make.sh
@@ -1,17 +1,17 @@
 
-### build 64 bit version ###
-luajit -b -a x64 tynstat.lua tynstat.o
+### build 64 bit versions ###
+luajit -b -a x64 src/tynstat.lua tynstat.o
 luajit -b -a x64 -n main -e "require('tynstat'); main();"    main.o
 luajit -b -a x64 -n main -e "require('tynstat'); daemain();" daemain.o
-gcc -m64 -Wl,-E main.o    tynstat.o llj_64bit.a -lm -ldl -o tynstat_64
-gcc -m64 -Wl,-E daemain.o tynstat.o llj_64bit.a -lm -ldl -o tynstad_64
+gcc -m64 -Wl,-E main.o    tynstat.o lib/llj_64bit.a -lm -ldl -o bin/64bit/tynstat
+gcc -m64 -Wl,-E daemain.o tynstat.o lib/llj_64bit.a -lm -ldl -o bin/64bit/tynstad
 rm tynstat.o main.o daemain.o
 
-### build 32 bit version ###
-luajit -b -a x86 tynstat.lua tynstat.o
+### build 32 bit versions ###
+luajit -b -a x86 src/tynstat.lua tynstat.o
 luajit -b -a x86 -n main -e "require('tynstat'); main();"    main.o
 luajit -b -a x86 -n main -e "require('tynstat'); daemain();" daemain.o
-gcc -m32 -Wl,-E main.o    tynstat.o llj_32bit.a -lm -ldl -o tynstat_32
-gcc -m32 -Wl,-E daemain.o tynstat.o llj_32bit.a -lm -ldl -o tynstad_32
+gcc -m32 -Wl,-E main.o    tynstat.o lib/llj_32bit.a -lm -ldl -o bin/32bit/tynstat
+gcc -m32 -Wl,-E daemain.o tynstat.o lib/llj_32bit.a -lm -ldl -o bin/32bit/tynstad
 rm tynstat.o main.o daemain.o
 
