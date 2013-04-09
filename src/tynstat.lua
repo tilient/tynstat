3478a0b0904de8e76565ab0cdc1acd62ee20b736
wiffel <wiffel@tilient.org>
2013-04-09 22:13:25 +0200

    small refactorings

--- src/tynstat.lua
+++ src/tynstat.lua
@@ -3,9 +3,7 @@
 ----------------------------------------------------------------------------
 --
 -- Tiny Stats Server
---
 --   github.com/wiffel/tynstat
---
 --   wiffel@tilient.org
 --
 ----------------------------------------------------------------------------
@@ -14,17 +12,8 @@ local ffi = require("ffi");
 
 ffi.cdef [[
 
-  typedef uint32_t pid_t;
-  typedef uint32_t mode_t;
   typedef uint32_t size_t;
-  typedef uint32_t off_t;
-  typedef intptr_t  ssize_t;
-  typedef uint32_t fd_set;
-
-  typedef struct {
-    long int tv_sec;  // second
-    long int tv_usec; // micro seconds
-  } timeval;
+  typedef intptr_t ssize_t;
 
   typedef struct {
     uint16_t sin_family;
@@ -69,29 +58,34 @@ ffi.cdef [[
 
   int close(int fd);
   ssize_t read(int fd, void *buf, size_t count);
-  ssize_t write(int fd, const void *buf, size_t count);
   ssize_t send(int fd, const void *buf, size_t count, int flags);
-  int select(int nfds, fd_set *readfds, fd_set *writefds,
-             fd_set *exceptfds, timeval *timeout);
 
   void usleep(uint32_t usecs);
   int daemon(int nochdir, int noclose);
-  pid_t fork();
-  int mkfifo(const char *pathname, mode_t mode); 
-  int remove(const char *pathname);
-  int open(const char *pathname, int flags);
 
 ]];
 
 C = ffi.C;
 
 ----------------------------------------------------------------------------
+--- Tools ------------------------------------------------------------------
+----------------------------------------------------------------------------
+
+function collect (t, obj)
+  t[#t+1] = obj;
+end
+
+function collectAll (t, objs)
+  for _, obj in ipairs(objs) do
+    collect(t, obj);
+  end
+end
+
+----------------------------------------------------------------------------
 --- TCP/IP -----------------------------------------------------------------
 ----------------------------------------------------------------------------
 
-local EAGAIN        = 11;
 local AF_INET       = 2;
-local AF_INET6      = 10;
 local SOCK_STREAM   = 1;
 local SO_REUSEDDR   = 2;
 local SOL_SOCKET    = 1;
@@ -134,7 +128,7 @@ function listenSocket(port)
   return s;
 end
 
-function handleIncomingConnections(port, handler)
+function serveRequestsOnPort(port, handler)
   local listen_socket = listenSocket(port);
   while true do
     local s = C.accept4(listen_socket, nil, nil, 0);
@@ -205,14 +199,20 @@ function writeString(s, str)
   writeBytes(s, buffer, nrOfBytes)
 end
 
+function writeStrings(s, strs)
+  for _, str in ipairs(strs) do
+    writeString(s, tostring(str));
+  end
+end
+
 ----------------------------------------------------------------------------
---- Stats ------------------------------------------------------------------
+--- Collection statistics --------------------------------------------------
 ----------------------------------------------------------------------------
 
 function collectUptimeStats(stats)
   local stts = {};
   stats.uptime = stts; 
-  local str  = io.open("/proc/uptime"):read("*a");
+  local str = io.open("/proc/uptime"):read("*a");
   stts.seconds, stts.idle = str:match("([%d%.]+)%s*([%d%.]+)");
   stts.seconds = tonumber(stts.seconds);
   stts.idle = tonumber(stts.idle);
@@ -245,7 +245,7 @@ function collectDfStats(stats)
   pr:read("*line");
   for str in pr:lines() do
     local st = {};
-    stts[#stts+1] = st;
+    collect(stts, st);
     st.filesystem, st.size, st.used, st.avail, st.usepercentage, st.mount =
       str:match("([%w/]+)%s+([%w%.]+)%s+([%w%.]+)%s+" ..  
                 "([%w%.]+)%s+([%w%%]+)%s+(.+)$");
@@ -260,7 +260,7 @@ function collectPsStats(stats)
   pr:read("*line");
   for str in pr:lines() do
     local st = {};
-    stts[#stts+1] = st;
+    collect(stts, st);
     st.cpu, st.mem, st.vsize, st.rss, st.cputime, st.user, st.args =
       str:match("(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(.-)%s*$");
     st.cpu   = 0.01 * tonumber(st.cpu);
@@ -281,52 +281,47 @@ function collectStats()
 end
 
 ----------------------------------------------------------------------------
+--- Stats to HTML ----------------------------------------------------------
+----------------------------------------------------------------------------
 
 function statObj2Html(obj, tstr, indent, d0, d1)
   if not obj then
-    tstr[#tstr+1] = "nil";
+    collect(tstr, "nil");
   else
     local objType = type(obj);
     if objType == "boolean" then
-      tstr[#tstr+1] = obj and "true" or "false";
+      collect(tstr, obj and "true" or "false");
     elseif objType == "string" then
-      tstr[#tstr+1] = obj;
+      collect(tstr, obj);
     elseif objType == "number" then
-      tstr[#tstr+1] = tostring(obj)
+      collect(tstr, tostring(obj));
     elseif objType == "table" then
-      local d0 = d0 or "d0";
-      local d1 = d1 or "d1";
+      local d0, d1 = d0 or "d0", d1 or "d1";
       local indent = indent or 0;
       local istr = string.rep(" ", indent);
-      tstr[#tstr+1] = "\r\n";
-      tstr[#tstr+1] = istr;
-      tstr[#tstr+1] = "<table>\r\n";
+      collectAll(tstr, { "\r\n", istr, "<table>\r\n" });
       for k, v in pairs(obj) do
         d1, d0 = d0, d1;
-        tstr[#tstr+1] = istr;
-        tstr[#tstr+1] = "  <tr class='";
-        tstr[#tstr+1] = d0;
-        tstr[#tstr+1] = "'><td class='l'>";
+        collectAll(tstr, {istr, "  <tr class='", d0, "'><td class='l'>"});
         statObj2Html(k, tstr, indent + 2, d0, d1);
-        tstr[#tstr+1] = "</td><td class='r'>";
+        collect(tstr, "</td><td class='r'>");
         statObj2Html(v, tstr, indent + 2, d0, d1);
-        tstr[#tstr+1] = "</td></tr>\r\n";
+        collect(tstr, "</td></tr>\r\n");
       end
-      tstr[#tstr+1] = istr;
-      tstr[#tstr+1] = "</table>";
+      collectAll(tstr, { istr; "</table>" });
     else
-      tstr[#tstr+1] = "???";
+      collect(tstr, "???");
     end
   end
 end
 
 function stats2html(stats)
   local tstr = {};
-  tstr[#tstr+1] = [[<!doctype html>
+  collect(tstr, [[<!doctype html>
 <html>
   <head>
     <meta charset='utf-8'>
-    <meta name='description' content='tenili'>
+    <meta name='description' content='tynstat'>
     <meta name='viewport' content='width=device-width'>
     <style type='text/css'>
       table { background-color : #ffffff; 
@@ -335,54 +330,54 @@ function stats2html(stats)
               cellspacing : 1px }
       tr.d0 { background-color: #eeeeee; }
       tr.d1 { background-color: #dddddd; }
-      td.l { vertical-align : top; 
-             text-align : right;
-             width : 20px; }
-      td.r { vertical-align : top; 
-             text-align : left;
-             width : 80px; }
+      td.l  { vertical-align : top; 
+              text-align : right;
+              width : 20px; }
+      td.r  { vertical-align : top; 
+              text-align : left;
+              width : 80px; }
     </style>
     <meta http-equiv='cleartype' content='on'>
   </head>
   <body>
-    <center><table style='width:600px;'><tr><td> ]];
+    <center><table style='width:600px;'><tr><td> ]]);
   statObj2Html(stats, tstr, 6);
-  tstr[#tstr+1] = [[ </td></tr></table></center>
+  collect(tstr, [[ </td></tr></table></center>
   </body>
-</html>]];
+</html>]]);
   return table.concat(tstr);
 end
 
+----------------------------------------------------------------------------
+--- Stats to JSON ----------------------------------------------------------
 ----------------------------------------------------------------------------
 
 function statObj2Json(obj, tstr, indent)
   local tstr = tstr or {};
   if not obj then
-    tstr[#tstr+1] = "null";
+    collect(tstr, "null");
   else
     local objType = type(obj);
     if objType == "boolean" then
-      tstr[#tstr+1] = obj and "true" or "false";
+      collect(tstr, obj and "true" or "false");
     elseif objType == "string" then
-      tstr[#tstr+1] = "\"";
-      tstr[#tstr+1] = obj;
-      tstr[#tstr+1] = "\"";
+      collectAll(tstr, { "\"", obj, "\"" });
     elseif objType == "number" then
-      tstr[#tstr+1] = tostring(obj);
+      collect(tstr, tostring(obj));
     elseif objType == "table" then
       local indent = indent or 2;
       local istr = string.rep(" ", indent);
-      tstr[#tstr+1] = "{\r\n";
+      collect(tstr, "{\r\n");
       for k, v in pairs(obj) do
-        tstr[#tstr+1] = istr;
+        collect(tstr, istr);
         statObj2Json(k, tstr, indent + 2);
-        tstr[#tstr+1] = " : ";
+        collect(tstr, " : ");
         statObj2Json(v, tstr, indent + 2);
-        tstr[#tstr+1] = ",\r\n";
+        collect(tstr, ",\r\n");
       end
-      tstr[#tstr] = " }";
+      collect(tstr,  " }");
     else
-      tstr[#tstr+1] = "\"???\"";
+      collect(tstr, "\"???\"");
     end
   end
   return tstr;
@@ -393,32 +388,7 @@ function stats2json(stats)
 end
 
 ----------------------------------------------------------------------------
-
-function handler(s)
-  local uri = readLine(s):match("GET%s+([%w/%.]+)%s+") or "/";
-
-  -- ignore all the headers
-  local cnt = 5000;
-  repeat cnt = cnt - 1; until (cnt < 0) or (readLine(s):len() <= 0);
-
-  if uri == "/html" then
-    writeString(s, "HTTP/1.0 200 OK\r\n");
-    writeString(s, "Content-Type: text/html\r\n");
-    str = stats2html(collectStats());
-    writeString(s, "Content-Length: " .. str:len() .. "\r\n\r\n");
-    writeString(s, str);
-  elseif uri == "/json" then
-    writeString(s, "HTTP/1.0 200 OK\r\n");
-    writeString(s, "Content-Type: application/json\r\n");
-    str = stats2json(collectStats());
-    writeString(s, "Content-Length: " .. str:len() .. "\r\n\r\n");
-    writeString(s, str);
-  else
-    writeString(s, "HTTP/1.0 404 Not Found\r\n\r\n");
-  end
-end
-
-
+--- main -------------------------------------------------------------------
 ----------------------------------------------------------------------------
 
 config = {
@@ -426,14 +396,13 @@ config = {
   daemonize = false;
 };
 
-function handleArgs()
+function handleCommandLineArguments()
   local i = 1;
   local cmd = arg[i];
   while cmd do
     if cmd == "-d" then
       config.daemonize = true;
-    end
-    if cmd == "-p" then
+    elseif cmd == "-p" then
       i = i + 1;
       local port = arg[i];
       config.port = port and tonumber(port) or config.port;
@@ -443,15 +412,37 @@ function handleArgs()
   end
 end
 
-function main()
-  handleArgs();
-  if config.daemonize then
-    C.daemon(0, 0);
+function handleStatsRequest (s)
+  local uri = readLine(s):match("GET%s+([%w/%.]+)%s+") or "/";
+
+  local cnt = 5000;     -- ignore all the header fields
+  repeat cnt = cnt - 1; until (cnt < 0) or (readLine(s):len() <= 0);
+
+  if uri == "/html" then
+    local str = stats2html(collectStats());
+    writeStrings(s, { "HTTP/1.0 200 OK\r\n",
+                      "Content-Type: text/html\r\n",
+                      "Content-Length: ", str:len(), "\r\n\r\n",
+                      str });
+  elseif uri == "/json" then
+    local str = stats2json(collectStats());
+    writeStrings(s, { "HTTP/1.0 200 OK\r\n",
+                      "Content-Type: application/json\r\n",
+                      "Content-Length: ", str:len(), "\r\n\r\n",
+                      str });
+  else
+    writeString(s, "HTTP/1.0 404 Not Found\r\n\r\n");
   end
-  handleIncomingConnections(config.port, handler);
 end
 
-main();
+----------------------------------------------------------------------------
+
+handleCommandLineArguments();
+if config.daemonize then
+  C.daemon(0, 0);
+end
+serveRequestsOnPort(config.port, handleStatsRequest);
+os.exit(); -- we should never be able to get to here.
 
 ----------------------------------------------------------------------------
 
